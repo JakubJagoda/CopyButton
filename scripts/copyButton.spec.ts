c6426a4d2013134e60afaecb8fa19d3b6736ba84
Kuba Jagoda <kuba.jagoda+github@gmail.com>
2015-06-15 10:43:41 +0200

    Manual copy handling

--- scripts/copyButton.spec.ts
+++ scripts/copyButton.spec.ts
@@ -10,95 +10,131 @@ describe('Component: copyButton', () => {
         element = document.createElement('button');
     });
 
-    describe('Instantiation', () => {
-        it('should compile bare HTML element', () => {
-            expect(() => {
-                copyButtonInstance = new CopyButton(element);
-            }).not.toThrow();
+    it('should compile bare HTML element', () => {
+        expect(() => {
+            copyButtonInstance = new CopyButton(element);
+        }).not.toThrow();
+    });
+
+    describe('zeroclipboard', () => {
+        let handlers;
+        let zeroClipboardInstanceMock;
+        beforeEach(() => {
+            handlers = {};
+            zeroClipboardInstanceMock = {
+                on: jasmine.createSpy('').and.callFake((evt, handler) => {
+                    handlers[evt] = handler;
+                }),
+                destroy: jasmine.createSpy('')
+            };
+
+            spyOn(window, 'ZeroClipboard').and.returnValue(zeroClipboardInstanceMock);
+        });
+
+        it('should install zeroclipboard and use it as a copy handler first', () => {
+            copyButtonInstance = new CopyButton(element);
+            expect(ZeroClipboard).toHaveBeenCalledWith(element);
+        });
+
+        it('should call destroy if error was reported by zeroclipboard', () => {
+            copyButtonInstance = new CopyButton(element);
+            handlers['error']();
+            expect(zeroClipboardInstanceMock.destroy).toHaveBeenCalled();
+        });
+
+        it('should install custom copy method if error was reported by zeroclipboard', () => {
+            copyButtonInstance = new CopyButton(element);
+            element.addEventListener = jasmine.createSpy('');
+            handlers['error']();
+            expect(element.addEventListener).toHaveBeenCalledWith('click', jasmine.any(Function));
         });
     });
 
-    describe('copying data using template', () => {
-        describe('zeroclipboard', () => {
-            let handlers;
-            let zeroClipboardInstanceMock;
-            beforeEach(() => {
-                handlers = {};
-                zeroClipboardInstanceMock = {
-                    on: jasmine.createSpy('').and.callFake((evt, handler) => {
-                        handlers[evt] = handler;
-                    }),
-                    destroy: jasmine.createSpy('')
-                };
+    describe('custom copy methods (zeroclipboard unavailable)', () => {
+        let _ZeroClipboard,
+            _clipboardData;
 
-                spyOn(window, 'ZeroClipboard').and.returnValue(zeroClipboardInstanceMock);
-            });
+        beforeEach(() => {
+            _ZeroClipboard = ZeroClipboard;
+            _clipboardData = window.clipboardData;
+            window.$$CopyButtonUserscriptEnabled = false;
 
-            it('should install zeroclipboard and use it as a copy handler first', () => {
-                copyButtonInstance = new CopyButton(element);
-                expect(ZeroClipboard).toHaveBeenCalledWith(element);
-            });
-
-            it('should call destroy if error was reported by zeroclipboard', () => {
-                copyButtonInstance = new CopyButton(element);
-                handlers['error']();
-                expect(zeroClipboardInstanceMock.destroy).toHaveBeenCalled();
-            });
-
-            it('should install custom copy method if error was reported by zeroclipboard', () => {
-                copyButtonInstance = new CopyButton(element);
-                element.addEventListener = jasmine.createSpy('');
-                handlers['error']();
-                expect(element.addEventListener).toHaveBeenCalledWith('click', jasmine.any(Function));
-            });
+            window['ZeroClipboard'] = undefined;
         });
 
-        describe('custom copy methods (zeroclipboard unavailable)', () => {
-            let _ZeroClipboard,
+        afterEach(() => {
+            window['ZeroClipboard'] = _ZeroClipboard;
+            window.clipboardData = _clipboardData;
+        });
+
+        it('should copy text on click using userscript as first method', () => {
+            window.$$CopyButtonUserscriptEnabled = true;
+            const locationOrigin = location.protocol + "//" + location.hostname + (location.port ? ':' + location.port : '');
+            spyOn(window, 'postMessage');
+            copyButtonInstance = new CopyButton(element);
+            copyButtonInstance.setCopyData('abc');
+            element.click();
+            expect(window.postMessage).toHaveBeenCalledWith({
+                $$CopyButtonDataToCopy: 'abc'
+            }, locationOrigin);
+        });
+
+        it('should copy text on click using clipboardData if userscript is not available', () => {
+            window.clipboardData = jasmine.createSpyObj('', ['setData']);
+            copyButtonInstance = new CopyButton(element);
+            copyButtonInstance.setCopyData('abc');
+            element.click();
+            expect(window.clipboardData.setData).toHaveBeenCalledWith('Text', 'abc')
+        });
+
+        it('should copy text on click using document.execCommand if clipboardData is not available', () => {
+            window.clipboardData = undefined;
+            spyOn(document, 'queryCommandSupported').and.returnValue(true);
+            spyOn(document, 'execCommand').and.returnValue(true);
+
+            copyButtonInstance = new CopyButton(element);
+            copyButtonInstance.setCopyData('abc');
+            element.click();
+            expect(document.execCommand).toHaveBeenCalledWith('copy');
+        });
+
+        describe('manual copy', () => {
+            let manualCtrlCElement,
                 _clipboardData;
 
             beforeEach(() => {
-                _ZeroClipboard = ZeroClipboard;
                 _clipboardData = window.clipboardData;
-                window.$$CopyButtonUserscriptEnabled = false;
+                window.clipboardData = undefined;
+                spyOn(document, 'queryCommandSupported').and.returnValue(false);
 
-                window['ZeroClipboard'] = undefined;
+                manualCtrlCElement = document.createElement('div');
+                document.body.appendChild(manualCtrlCElement);
             });
 
             afterEach(() => {
-                window['ZeroClipboard'] = _ZeroClipboard;
                 window.clipboardData = _clipboardData;
+                document.body.removeChild(manualCtrlCElement);
             });
 
-            it('should copy text on click using userscript as first method', () => {
-                window.$$CopyButtonUserscriptEnabled = true;
-                const locationOrigin = location.protocol + "//" + location.hostname + (location.port ? ':' + location.port : '');
-                spyOn(window, 'postMessage');
-                copyButtonInstance = new CopyButton(element);
+            it('should create manual copy possibility when all above are not available', () => {
+                spyOn(CopyButton, 'isTouchDisplay').and.returnValue(false);
+                copyButtonInstance = new CopyButton(element, manualCtrlCElement);
                 copyButtonInstance.setCopyData('abc');
                 element.click();
-                expect(window.postMessage).toHaveBeenCalledWith({
-                    $$CopyButtonDataToCopy: 'abc'
-                }, locationOrigin);
+
+                expect(manualCtrlCElement.classList).toContain('copy-button-visible');
+                expect($(manualCtrlCElement).find('.copy-button-data-to-copy').val()).toBe('abc');
+                expect($(manualCtrlCElement).find('.copy-button-data-to-copy').css('opacity')).toBe('0');
             });
 
-            it('should copy text on click using clipboardData if userscript is not available', () => {
-                window.clipboardData = jasmine.createSpyObj('', ['setData']);
-                copyButtonInstance = new CopyButton(element);
+            it('should not hide the textarea when the device has touch input', () => {
+                spyOn(CopyButton, 'isTouchDisplay').and.returnValue(true);
+
+                copyButtonInstance = new CopyButton(element, manualCtrlCElement);
                 copyButtonInstance.setCopyData('abc');
                 element.click();
-                expect(window.clipboardData.setData).toHaveBeenCalledWith('Text', 'abc')
-            });
 
-            it('should copy text on click using document.execCommand if clipboardData is not available', () => {
-                window.clipboardData = undefined;
-                spyOn(document, 'queryCommandSupported').and.returnValue(true);
-                spyOn(document, 'execCommand').and.returnValue(true);
-
-                copyButtonInstance = new CopyButton(element);
-                copyButtonInstance.setCopyData('abc');
-                element.click();
-                expect(document.execCommand).toHaveBeenCalledWith('copy');
+                expect($(manualCtrlCElement).find('.copy-button-data-to-copy').css('opacity')).not.toBe('0');
             });
         });
     });
--- scripts/copyButton.ts
+++ scripts/copyButton.ts
@@ -11,16 +11,29 @@ interface Window {
 const enum COPY_METHOD {
     USERSCRIPT,
     CLIPBOARD_DATA,
-    DOCUMENT_EXECCOMMAND
+    DOCUMENT_EXECCOMMAND,
+    MANUAL_CTRL_C
 }
 
 class CopyButton {
     private copyData:string;
-    private element:HTMLElement;
+    private clickToCopyElement:HTMLElement;
+    private manualCtrlCElement:HTMLElement;
     private zeroClipboardClient:ZeroClipboard;
 
-    constructor(element:HTMLElement) {
-        this.element = element;
+    private static MANUAL_CTRL_C_VISIBLE_CLASS = 'copy-button-visible';
+    private static MANUAL_CTRL_C_HIDDEN_CLASS = 'copy-button-hidden';
+    private static MANUAL_CTRL_C_TEXTAREA_CLASS = 'copy-button-data-to-copy';
+
+    constructor(clickToCopyElement:HTMLElement, manualCtrlCElement?:HTMLElement) {
+        this.clickToCopyElement = clickToCopyElement;
+        this.manualCtrlCElement = manualCtrlCElement;
+
+        if(this.manualCtrlCElement) {
+            this.manualCtrlCElement.classList.add(CopyButton.MANUAL_CTRL_C_HIDDEN_CLASS);
+            this.manualCtrlCElement.classList.remove(CopyButton.MANUAL_CTRL_C_VISIBLE_CLASS);
+        }
+
         if (typeof ZeroClipboard !== 'undefined') {
             this.installZeroClipboard();
             this.zeroClipboardClient.on('error', () => {
@@ -34,14 +47,14 @@ class CopyButton {
     }
 
     private installZeroClipboard():void {
-        this.zeroClipboardClient = new ZeroClipboard(this.element);
+        this.zeroClipboardClient = new ZeroClipboard(this.clickToCopyElement);
         this.zeroClipboardClient.on('copy', (e) => {
             e.clipboardData.setData('text/plain', this.getDataToCopy());
         });
     }
 
     private installCustomCopyMethod():void {
-        this.element.addEventListener('click', () => {
+        this.clickToCopyElement.addEventListener('click', () => {
             this.copy(this.getDataToCopy());
         });
     }
@@ -59,9 +72,8 @@ class CopyButton {
             chosenCopyMethod = COPY_METHOD.CLIPBOARD_DATA;
         } else if (document.execCommand && document.queryCommandSupported('copy')) {
             chosenCopyMethod = COPY_METHOD.DOCUMENT_EXECCOMMAND;
-        } else {
-            //@todo add other methods
-            throw new Error('No available copy API');
+        } else if (this.manualCtrlCElement) {
+            chosenCopyMethod = COPY_METHOD.MANUAL_CTRL_C;
         }
 
         this.COPY_FUNCTIONS[chosenCopyMethod](str);
@@ -97,6 +109,58 @@ class CopyButton {
             window.postMessage({
                 $$CopyButtonDataToCopy: str
             }, locationOrigin);
+        },
+        [COPY_METHOD.MANUAL_CTRL_C]: (str:string) => {
+            this.manualCtrlCElement.classList.remove(CopyButton.MANUAL_CTRL_C_HIDDEN_CLASS);
+            this.manualCtrlCElement.classList.add(CopyButton.MANUAL_CTRL_C_VISIBLE_CLASS);
+
+            const hiddenInput = document.createElement('textarea');
+            if(!CopyButton.isTouchDisplay()) {
+                //this doesn't guarantee that user doesn't have a physical keyboard, but it's more probable they won't have it
+                //when they have a touch input device
+                hiddenInput.setAttribute('style', 'width: 1px; height: 1px; padding: 0px; margin: 0px; opacity: 0;');
+            }
+            hiddenInput.value = str;
+            hiddenInput.classList.add(CopyButton.MANUAL_CTRL_C_TEXTAREA_CLASS);
+            this.manualCtrlCElement.appendChild(hiddenInput);
+            hiddenInput.focus();
+            hiddenInput.select();
+
+            const handleCopyOnce = (e) => {
+                if(e.target !== hiddenInput) {
+                    return;
+                }
+
+                hiddenInput.parentNode.removeChild(hiddenInput);
+                this.manualCtrlCElement.classList.remove(CopyButton.MANUAL_CTRL_C_VISIBLE_CLASS);
+                this.manualCtrlCElement.classList.add(CopyButton.MANUAL_CTRL_C_HIDDEN_CLASS);
+                document.removeEventListener('keyup', handleCopyOnce);
+            };
+
+            const hideCtrlCElement = (e) => {
+                if(e.target === hiddenInput || e.target === this.manualCtrlCElement) {
+                    return;
+                }
+
+                hiddenInput.parentNode.removeChild(hiddenInput);
+                this.manualCtrlCElement.classList.remove(CopyButton.MANUAL_CTRL_C_VISIBLE_CLASS);
+                this.manualCtrlCElement.classList.add(CopyButton.MANUAL_CTRL_C_HIDDEN_CLASS);
+                document.removeEventListener('click', hideCtrlCElement);
+            };
+
+            if(!CopyButton.isTouchDisplay()) {
+                //@todo is it possible that this event will be added and never removed, causing them to stack?
+                document.addEventListener('keyup', handleCopyOnce);
+            } else {
+                setTimeout(() => {
+                    document.addEventListener('click', hideCtrlCElement);
+                });
+            }
         }
+    };
+
+    //@todo make private, currently public for easy mocking
+    static isTouchDisplay():boolean {
+        return !!('ontouchstart' in window || navigator.msMaxTouchPoints);
     }
 }
